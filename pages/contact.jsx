74bb73df92addbd4253ed47ebcf61a1e84dbec63
gass2104-sudo <gass2104@gmail.com>
2025-11-19 12:32:06 -0300

    Add files via upload

---
+++ pages/contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact(){
+  return (
+    <div className="max-w-4xl mx-auto p-8">
+      <h2 className="text-2xl font-bold mb-4">Fale conosco</h2>
+      <p>Envie sugestões, correções ou queira colaborar: contato@sporthubbrasil.com</p>
+    </div>
+  )
+}
